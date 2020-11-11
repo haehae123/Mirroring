897dcaec9f495bfca67c205104d567eb00cd0eff
Matthias Louis Vogt <matthias.vogt@campus.tu-berlin.de>
2020-11-11 13:46:49 +0100

    added cleaning script

---
+++ clean.sh
@@ -0,0 +1,13 @@
+rm -f ChatCoder2/datapacks/*.json
+rm -f ChatCoder2/csv/*.csv
+
+rm -f PAN12/datapacks/*.json
+rm -f PAN12/csv/*.csv
+
+rm -f PANC/datapacks/*.json
+rm -f PANC/csv/*.csv
+
+rm -f VTPAN/datapacks/*.json
+rm -f VTPAN/csv/*.csv
+
+echo "Removed all generated files"
