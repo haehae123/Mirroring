b8bc5f3621d52e995074588e5e10465447507bc6
Matthias Louis Vogt <matthias.vogt@campus.tu-berlin.de>
2020-11-11 20:37:30 +0100

    added statistics and moves isGood to util.py

--- create_csv.py
+++ create_csv.py
@@ -13,6 +13,7 @@ import numpy as np
 import json
 from util import contentToString, getSegments
 import csv
+from util import isGood
 
 parser = argparse.ArgumentParser(description='Create a tsv dataset from a datapack')
 parser.add_argument(
@@ -33,53 +34,19 @@ print("\n---           Arguments           ---")
 for key, val in args.__dict__.items(): print("%20s: %s" % (key, val))
 print()
 
-def isGood(segment): # which segments should be filtered
-	if args.dataset == "PANC":
-		# Filters segments with more than 150 nonempty messages.
-		# The few unbehaved PAN12 segments with >150 messages are already
-		# filtered in PANC/create_datapack.py, the negative segments with non
-		# nonempty messages are as well. This code is only for the segments from
-		# the complete positive chats originally from CC2.
-		numOfNonemptyMessages = len([msg for msg in segment if msg is not None])
-		return 1 <= numOfNonemptyMessages <= 150
-	if args.dataset == "VTPAN": return True # don't filter these
-	return True # for other datasets also allow all segments
-
 def writeCSV(file, datapack, datasetType):
 	writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_ALL)
 	writer.writerow(["label", "chatName", "segment"]) # header
 
 	for chatName, chat in datapack["chats"].items():
 		for i, segment in enumerate(getSegments(chat)):
-			if not isGood(segment): continue
+			if not isGood(segment, args): continue
 
 			# add segment number if segment is not whole chat
 			segmentName = "%s-%s" % (chatName, i) \
 				if len(segment) != len(chat["content"]) else chatName
 
 			writer.writerow([chat["className"], segmentName, contentToString(segment)])
-
-def printStatistics(datapack, datasetType):
-
-	chatClassCount = Counter()
-	segmentClassCount = Counter()
-
-	for chatName, chat in datapack["chats"].items():
-		chatClassCount.update([chat["className"]])
-		for i, segment in enumerate(getSegments(chat)):
-			if not isGood(segment): continue
-
-			segmentClassCount.update([chat["className"]])
-
-	print("In %s:" % datasetType)
-	total = segmentClassCount["predator"] + segmentClassCount["non-predator"]
-	print("\tnumber of segments: %s" % total)
-	print("\tchatClassCount = %s" % chatClassCount)
-	print("\tsegmentClassCount = %s" % segmentClassCount)
-	print("\tpredator class ratio: predator/all = %s/%s = %s%%" % (
-		segmentClassCount["predator"], total,
-		np.round(segmentClassCount["predator"]/total*100, 2)
-	))
 
 for datasetType in ["train", "test"]:
 	datapackPath = "%s/datapacks/datapack-%s-%s.json" % (
@@ -92,6 +59,5 @@ for datasetType in ["train", "test"]:
 		# write TSV
 		with open(csvPath, 'w', newline='') as file:
 			writeCSV(file, datapack, datasetType)
-			printStatistics(datapack, datasetType)
 
 print("\nSuccessfully wrote all csv files")
