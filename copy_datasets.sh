5ab2d29f6391cd65d9ca429dc4138e4059743757
Matthias Vogt <matthias@vo.gt>
2021-06-17 17:10:48 +0200

    Fixed copy datasets script

--- copy_datasets.sh
+++ copy_datasets.sh
@@ -1,10 +1,13 @@
-mkdir -p ../early-sexual-predator-detection-lab/datasets/{VTPAN,PANC}/
+read -p "Enter the location of the eSPD Laboratory repository [../eSPD-lab/]: " name
+lab=${name:-../eSPD-lab/}
+
+mkdir -p "$lab/datasets/"{VTPAN,PANC}
 
 for dataset in PANC VTPAN; do
         for datasetType in train test; do
                 echo "copying $dataset $datasetType"
-                cp "$dataset/datapacks/datapack-$dataset-$datasetType.json" ../early-sexual-predator-detection-lab/datasets/$dataset/
-                cp "$dataset/csv/$dataset-$datasetType.csv" ../early-sexual-predator-detection-lab/datasets/$dataset/
+                cp "$dataset/datapacks/datapack-$dataset-$datasetType.json" "$lab/datasets/$dataset/"
+                cp "$dataset/csv/$dataset-$datasetType.csv" "$lab/datasets/$dataset/"
         done
 done
 
