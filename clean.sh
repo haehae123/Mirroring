4435f788c1c9f93f2b838dd27b05dd52268385e2
Matthias Vogt <matthias@vo.gt>
2021-06-17 17:10:22 +0200

    made clean more aggressive >:D

--- clean.sh
+++ clean.sh
@@ -1,13 +1,13 @@
-rm -f ChatCoder2/datapacks/*.json
-rm -f ChatCoder2/csv/*.csv
+rm -rf -f ChatCoder2/datapacks/
+rm -rf -f ChatCoder2/csv/
 
-rm -f PAN12/datapacks/*.json
-rm -f PAN12/csv/*.csv
+rm -rf -f PAN12/datapacks/
+rm -rf -f PAN12/csv/
 
-rm -f PANC/datapacks/*.json
-rm -f PANC/csv/*.csv
+rm -rf -f PANC/datapacks/
+rm -rf -f PANC/csv/
 
-rm -f VTPAN/datapacks/*.json
-rm -f VTPAN/csv/*.csv
+rm -rf -f VTPAN/datapacks/
+rm -rf -f VTPAN/csv/
 
 echo "Removed all generated files"
